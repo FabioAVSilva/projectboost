f75d6e0c8af3cf4646802706dd40863721563f3b
Fabio Silva <fabio.silva99@hotmail.com>
2025-08-09 16:53:44 +0100

    Adds basic player interaction
    Implements a counter that increments when the spacebar is pressed.
    Prints the number of times the spacebar has been pressed to the console.

--- player.gd
+++ player.gd
@@ -1,13 +1,16 @@
 extends Node3D
-
+var spaces: int = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	print("Hello World")
-	print("Don't Panic")
-	print(42)
+	
+	var my_number: int = 10
+	print(my_number)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	if Input.is_action_just_pressed("ui_accept"):
+		spaces = spaces + 1
+		print("Times spacebar was pressed:")
+		print(spaces)
