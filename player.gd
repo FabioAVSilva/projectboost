e5e00833c7f2f85a8a4d370281590606625b50dd
Fabio Silva <fabio.silva99@hotmail.com>
2025-08-09 17:05:21 +0100

    Adds basic player movement
    Implements rudimentary player controls for testing.
    
    Allows the player to move vertically and rotate using input actions, enabling basic navigation within the game environment.

--- player.gd
+++ player.gd
@@ -1,16 +1,13 @@
 extends Node3D
-var spaces: int = 0
-
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	
-	var my_number: int = 10
-	print(my_number)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	if Input.is_action_just_pressed("ui_accept"):
-		spaces = spaces + 1
-		print("Times spacebar was pressed:")
-		print(spaces)
+	if Input.is_action_pressed("ui_accept"):
+		position.y += delta
+		
+	if Input.is_action_pressed("ui_left"):
+		rotate_z(delta)
+		
+	if Input.is_action_pressed("ui_right"):
+		rotate_z(-delta)
