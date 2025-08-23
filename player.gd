e1ef09f6b385e2d6395274f4bb8c861cc6f9f003
Fabio Silva <fabio.silva99@hotmail.com>
2025-08-23 16:28:51 +0100

    Added VFX for boosting the rocket
    VFX will exit the bottom of the rocket when boosting, and left and right of the rocket when turning it

--- player.gd
+++ player.gd
@@ -11,22 +11,34 @@ var is_transitioning: bool = false
 @onready var explosion_audio: AudioStreamPlayer = $ExplosionAudio
 @onready var success_audio: AudioStreamPlayer = $SuccessAudio
 @onready var rocket_audio: AudioStreamPlayer3D = $RocketAudio
+@onready var booster_particles: GPUParticles3D = $BoosterParticles
+@onready var booster_particles_turn_left: GPUParticles3D = $BoosterParticlesTurnLeft
+@onready var booster_particles_turn_right: GPUParticles3D = $BoosterParticlesTurnRight
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	if Input.is_action_pressed("boost"):
 		apply_central_force(basis.y * delta * thrust)
+		booster_particles.emitting = true
 		if !rocket_audio.playing:
 			rocket_audio.play()
 	else:
+		booster_particles.emitting = false
 		rocket_audio.stop()
 		
 	if Input.is_action_pressed("rotate_left"):
 		apply_torque(Vector3(0.0, 0.0, torque) * delta)
+		booster_particles_turn_left.emitting = true
+	else:
+		booster_particles_turn_left.emitting = false
 		
 	if Input.is_action_pressed("rotate_right"):
 		apply_torque(Vector3(0.0, 0.0, -torque) * delta)
+		booster_particles_turn_right.emitting = true
+	else:
+		booster_particles_turn_right.emitting = false	
+	
 
 
 func _on_body_entered(body: Node) -> void:
